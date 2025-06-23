54dc4af9870c019864cb53514106c0a5a52f193c
Darryl James Cruz <darryljamescruz@gmail.com>
2025-06-23 16:04:46 -0700

    removed app.css, started off app.jsx as a blank slate, styling to be used with tailwindcss

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// entry point for react frontend
+// App.jsx will be the main component
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
