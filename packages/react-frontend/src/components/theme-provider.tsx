684dfc98e45c3bef118bfd006c551946b94b3338
Darryl James Cruz <darryljamescruz@gmail.com>
2025-07-04 23:00:14 -0700

    force light theme

--- packages/react-frontend/src/components/theme-provider.tsx
+++ packages/react-frontend/src/components/theme-provider.tsx
@@ -12,7 +12,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       {...props}
